98a0fd566418afa4a1f5c37178c9e7a01c66de38
Kosaczek13 <kosak12@interia.pl>
2019-12-12 09:39:49 +0100

    loop for

---
+++ bash/z5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#loop for 
+for x in j d t 
+do
+echo ${x}
+done
+
+for plik in *
+do
+echo ${plik}
+done
+#i++ - i=i+1 inkrementacja
+#i-- - i=i-1 dekrementacja
+#i++ - i=i+1
+#i=1 1<=3 -> true i=1+1
+#i=2 2<=3 -> true i=2+1
+#i=3 3<=3 -> true i=3+1
+#i=4 4<=3 -> false koniec
+for ((i=1; i<=3; i++))
+do
+echo ${i}
+done
+
+#loop while
+j=1
+
+while [ ${i} -le 100]; do
+echo -e "\0227"
+j$[i+1]
+done
